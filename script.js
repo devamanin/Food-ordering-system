8cc537d23511edb7aebda0c89b4c9954a2d2fb57
Aman Kumar <amansingh1032000@gmail.com>
2021-07-06 15:08:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
 let searchBox = document.getElementById("searchbox");
+console.log(searchBox.style.display);
+// let searchBut = document.getElementById('submitbut');
 let searchBut = document.getElementsByClassName("fa")[0];
+// searchBut.onclick = function(){
+//     searchBox.style.display = 'None';
+//     searchBox.value = "Hello World";
+// }
 let loggedin = false;
-let usernamebuy;
 function searchboxcss() {
   let displayValue = window.getComputedStyle(searchBox).display;
   if (displayValue == "none") {
     searchBox.style.display = "inline-block";
     document.getElementsByClassName("fa")[0].style.transform =
       "translate(0, 0)";
+    // document.getElementsByClassName('search-box-container')[0].style.animation = 'example 0.5s ease-in';
   } else {
     searchBox.style.display = "none";
     document.getElementsByClassName("fa")[0].style.transform =
       "translate(225px, 0)";
+    // document.getElementsByClassName('search-box-container')[0].style.animation-iteration-count = 'infinite';
   }
 }
+let usernamebuy;
 searchBut.addEventListener("click", searchboxcss);
 let foodBut1 = document.getElementsByClassName("food-type")[0];
 let foodBut2 = document.getElementsByClassName("food-type")[1];
 function foodVisual1() {
   foodBut1.style.borderStyle = "solid";
+  // foodBut1.style.borderColor = "rgb(247, 97, 71)";
+  // foodBut1.style.borderWidth = "2px";
   foodBut1.style.borderBottomWidth = "3px";
   foodBut1.style.borderBottomColor = "rgb(247, 97, 71)";
   foodBut2.style.borderBottomColor = "transparent";
+  // let slidingTagLiAfterStyle = document.createElement("style");
+  // slidingTagLiAfterStyle.innerHTML ="#main .header-container:after{background-color: rgb(243, 86, 165)}";
+  // document.head.appendChild(slidingTagLiAfterStyle);
 }
 function foodVisual2() {
   foodBut2.style.borderStyle = "solid";
+  // foodBut2.style.borderColor = "rgb(247, 97, 71)";
+  // foodBut2.style.borderWidth = "2px";
   foodBut2.style.borderBottomWidth = "3px";
   foodBut2.style.borderBottomColor = "rgb(247, 97, 71)";
   foodBut1.style.borderBottomColor = "transparent";
+  // let slidingTagLiAfterStyle = document.createElement("style");
+  // slidingTagLiAfterStyle.innerHTML ="#main .header-container:after{background-color: rgb(243, 86, 165)}";
+  // document.head.appendChild(slidingTagLiAfterStyle);
 }
 foodBut1.addEventListener("click", foodVisual1);
 foodBut2.addEventListener("click", foodVisual2);
 function loginfun() {
   var loginContainer = document.createElement("div");
   document.body.appendChild(loginContainer);
+  // loginContainer.classList.add("login-container");
   loginContainer.setAttribute("class", "login-container");
   var loginBox = document.createElement("div");
   document.getElementsByClassName("login-container")[0].appendChild(loginBox);
@@ -59,6 +78,10 @@ function loginfun() {
   let scrollpost = document.body.scrollTop;
   document.getElementsByClassName("login-container")[0].style.top =
     scrollpost + "px";
+  // function myfunc(){
+  //     document.getElementsByClassName('login-container')[0].style.display="none";
+  // }
+  // document.getElementsByClassName('login-container')[0].addEventListener('click', myfunc);
   function loginoutside(e) {}
   document.addEventListener("mouseup", function (e) {
     var container = document.getElementsByClassName("login-box")[0];
@@ -79,6 +102,7 @@ function loginfun() {
         url: "https://upper-eh-24269.herokuapp.com/login/",
         data: JSON.stringify(server_data),
         contentType: "application/json",
+        // dataType: 'json'
       });
       request.done(function (data) {
         if (data["status"] == true) {
@@ -86,6 +110,7 @@ function loginfun() {
             document.getElementsByClassName("login-container")[0];
           containerbox.remove();
           document.body.style.overflow = "visible";
+
           loggedin = true;
           alert("You are logged in successfully");
           document.getElementById("log-in").style.display = "none";
@@ -152,16 +177,19 @@ function signupfun() {
       let fullname = document.getElementById("fullname").value;
       let emailid = document.getElementsByClassName("emailid")[0].value;
       let password = document.getElementById("password").value;
+      // console.log(fullname + emailid + password);
       var server_data = {
         fullname: fullname,
         email: emailid,
         password: password,
       };
+      // console.log(server_data);
       var request = $.ajax({
         type: "POST",
         url: "https://upper-eh-24269.herokuapp.com/signup/",
         data: JSON.stringify(server_data),
         contentType: "application/json",
+        // dataType: 'json'
       });
       request.done(function (data) {
         if (data["status"] == "200") {
@@ -179,6 +207,19 @@ function signupfun() {
 
 let signBut = document.getElementById("sign-up");
 signBut.addEventListener("click", signupfun);
+// var secFood = document.createElement('section');
+// secFood.setAttribute('id', 'food-category');
+// document.body.appendChild(secFood);
+// var container = document.createElement('div');
+// container.setAttribute('class', 'container');
+// document.getElementById('food-category').appendChild(container);
+// var span = document.createElement('span')
+// span.innerHTML = "Inspiration for your first order";
+// document.getElementsByClassName('container')[0].appendChild(span);
+// var containerContent = document.createElement('div');
+// containerContent.setAttribute('class', 'container-content');
+// document.getElementsByClassName('container')[0].appendChild(containerContent);
+
 function viewCard() {
   let foodContent = document.createElement("div");
   foodContent.setAttribute("class", "food-content");
@@ -206,12 +247,16 @@ function viewCard() {
   document.getElementsByClassName("button1")[0].appendChild(button1);
   button1.innerHTML = "Buy";
 }
+// for (let index = 0; index < 4; index++) {
+//     viewCard(
 function buybutton(name) {
   if (!loggedin) {
     alert("Hello from button");
+    // foodnamebuy = document.getElementsByClassName('food-name')[0].innerHTML;
     console.log(name);
   }
 }
+
 function newtest(name, imagesrc, price, index) {
   var foodContent = document.createElement("div");
   foodContent.setAttribute("class", "food-content");
@@ -281,14 +326,16 @@ function newtest(name, imagesrc, price, index) {
             url: "https://upper-eh-24269.herokuapp.com/order/",
             data: JSON.stringify(server_data),
             beforeSend: function () {
-              document.body.style.overflow = "visible";
+              // document.body.style.overflow = "visible";
             },
             contentType: "application/json",
             complete: function () {
-              document.body.style.overflow = "hidden";
+              // document.body.style.overflow = "hidden";
             },
+            // dataType: 'json'
           });
           request.done(function (data) {
+            // console.log(data['status'])
             let ordercompleteBox = `<div class="order-complete-box">
                     <img src="./images/order.gif" alt="" class="img-order-complete">
                     <span class="order-complete">Hey, foody! your food is getting ready for the delivery</span>
@@ -312,14 +359,61 @@ function newtest(name, imagesrc, price, index) {
       }
     });
 }
+// $(".buybut").ready(function(){
+//     alert("Hello World")
+// })
+// for (let index = 0; index < 4; index++) {
+//     newtest("McDonald");
+// }
+// let buybutton = document.getElementsByClassName('buybut')[0];
+// if(buybutton){
+//     buybutton.addEventListener('click', function(){
+
+//     })
+// }
+
+// let content_loaded = false;
+// function testing(){
+//     if (content_loaded){
+//     if (loggedin==false){
+//         alert("Hello World")
+//     }
+// }
+// }
+function elementshide(){
+  document.getElementsByClassName('nav-bar')[0].style.display = "none";
+  document.getElementById('main').style.display = "none";
+  document.getElementById('food-category').style.display = "none";
+  document.getElementById('footer').style.display = "none";
+}
+function elementsunhide(){
+  document.getElementsByClassName('nav-bar')[0].style.display = "flex";
+  document.getElementById('main').style.display = "block";
+  document.getElementById('food-category').style.display = "block";
+  document.getElementById('footer').style.display = "block";
+}
 $(document).ready(function () {
   var server_data = { email: "hey@mail.com", password: "101010" };
   var request = $.ajax({
     type: "GET",
     url: "https://upper-eh-24269.herokuapp.com/",
     data: JSON.stringify(server_data),
-
+    beforeSend: function(){
+      document.body.scrollTop = "0px";
+      let loader = document.createElement('div');
+      loader.setAttribute('class', 'loader');
+      loader.innerHTML = `<div class="loader-container"><div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>`;
+      document.body.appendChild(loader);
+      elementshide();
+      document.body.style.overflowY = "hidden";
+    },
+    complete: function(){
+      document.getElementsByClassName('loader')[0].remove();
+      document.body.style.overflowY = "scroll";
+      elementsunhide();
+    },
     contentType: "application/json",
+    // dataType: 'json'
   });
   request.done(function (data) {
     console.log(data);
@@ -333,3 +427,56 @@ $(document).ready(function () {
     }
   });
 });
+
+// var url = "https://reqbin.com/echo/post/json";
+// var url = "http://127.0.0.1:5000/"
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", url);
+
+// xhr.setRequestHeader("Authorization", "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y");
+// xhr.setRequestHeader("Content-Type", "application/json");
+
+// xhr.onreadystatechange = function () {
+//    if (xhr.readyState === 4) {
+//       console.log(xhr.status);
+//       console.log(xhr.responseText + "Hello World");
+//    }};
+
+// var data = `{
+//   "Id": 12345,
+//   "Customer": "John Smith",
+//   "Quantity": 1,
+//   "Price": 10.00
+// }`;
+
+// xhr.send(data);
+
+// document.getElementsByClassName('buybut')[0].addEventListener('click', function(){
+//     var server_data = {"email": "hey@mail.com", "password": "101010"}
+//     var request = $.ajax({
+//         type: "POST",
+//         url: "http://127.0.0.1:5000/",
+//         data: JSON.stringify(server_data),
+//         contentType: "application/json",
+//         // dataType: 'json'
+//       })
+//       request.done(function(data){
+//           newtest('pizza', data);
+//       });
+// })
+
+// var url = "http://127.0.0.1:5000/"
+// var xhr = new XMLHttpRequest();
+// xhr.open("POST", url);
+
+// // xhr.setRequestHeader("Authorization", "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y");
+// // xhr.setRequestHeader("Content-Type", "application/json");
+
+// xhr.onreadystatechange = function () {
+//    if (xhr.readyState === 4) {
+//       console.log(xhr.status);
+//       console.log(xhr.responseText + "Hello World");
+//    }};
+
+// var data = JSON.stringify({"email": "hey@mail.com", "password": "101010"});
+// xhr.send(data);
